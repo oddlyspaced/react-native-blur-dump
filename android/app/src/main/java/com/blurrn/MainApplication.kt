7364524dec26803435091dc3d6a12cb4c70457f2
oddlyspaced <oddlyspaced1@gmail.com>
2025-07-29 15:21:14 +0530

    chore: base blur view setup

--- android/app/src/main/java/com/blurrn/MainApplication.kt
+++ android/app/src/main/java/com/blurrn/MainApplication.kt
@@ -1,6 +1,7 @@
 package com.blurrn
 
 import android.app.Application
+import com.blurrn.blurview.BlurViewPackage
 import com.facebook.react.PackageList
 import com.facebook.react.ReactApplication
 import com.facebook.react.ReactHost
@@ -18,6 +19,7 @@ class MainApplication : Application(), ReactApplication {
             PackageList(this).packages.apply {
               // Packages that cannot be autolinked yet can be added manually here, for example:
               // add(MyReactNativePackage())
+                add(BlurViewPackage())
             }
 
         override fun getJSMainModuleName(): String = "index"
